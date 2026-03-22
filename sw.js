11fe6b62ccc196a7afbbe4088cce23da60ded9cb
flatspace1112-sudo <flat_space1112@icloud.com>
2026-03-22 22:51:09 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'translingo-v3';
+const FILES = [
+  '/translingo/',
+  '/translingo/index.html',
+  '/translingo/manifest.json',
+  '/translingo/icon-translingo.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.url.includes('googleapis.com') || e.request.url.includes('workers.dev')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(res) {
+        var clone = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+        return res;
+      });
+    }).catch(function() {
+      return caches.match('/translingo/');
+    })
+  );
+});
